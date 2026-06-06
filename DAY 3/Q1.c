9d20b85e2a110706dec3f52f1639818ff042d3a6
Shreyansh Raj <shreyx61@gmail.com>
2026-06-07 01:36:14 +0530

    completed

---
+++ DAY 3/Q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1)
+    {
+        printf("Not Prime");
+    }
+    else
+    {
+        for(i = 2; i < n; i++)
+        {
+            if(n % i == 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 0)
+        {
+            printf("Prime Number");
+        }
+        else
+        {
+            printf("Not Prime");
+        }
+    }
+
+    return 0;
+}
